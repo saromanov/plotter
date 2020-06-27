40e7834112bf2134f53887bd90558637bfbbbdb3
Sergey <xxsmotur@gmail.com>
2020-06-27 20:46:51 +0500

    Fixign of line methods

--- plotter.go
+++ plotter.go
@@ -5,11 +5,16 @@ import (
 	"image/color"
 
 	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
+// LineOpts defines options for creating line plot
 type LineOpts struct {
-	XLabel string
-	YLabel string
+	XLabel   string
+	YLabel   string
+	Data     plotter.XYs
+	Filename string
 }
 
 // Plotter defines struct for project
@@ -21,25 +26,28 @@ type Plotter struct {
 func New() (*Plotter, error) {
 	p, err := plot.New()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	return &Plotter{
 		plot: p,
-	}
+	}, nil
 }
 
 // Line provides creating and saving of the line plot
 func (p *Plotter) Line(opts LineOpts) error {
-	p.Title.Text = fmt.Sprintf("Memory Plot of PID %d", 10)
-	p.X.Label.Text = opts.XLabel
-	p.Y.Label.Text = opts.YLabel
-	p.Add(plotter.NewGrid())
-	rssLine, err := plotter.NewLine(pts)
+	p.plot.Title.Text = fmt.Sprintf("Memory Plot of PID %d", 10)
+	p.plot.X.Label.Text = opts.XLabel
+	p.plot.Y.Label.Text = opts.YLabel
+	p.plot.Add(plotter.NewGrid())
+	rssLine, err := plotter.NewLine(opts.Data)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("unable to create new line: %v", err)
 	}
 	rssLine.LineStyle.Width = vg.Points(1)
 	rssLine.LineStyle.Color = color.RGBA{R: 100, G: 100, B: 0, A: 255}
-	p.Add(rssLine)
-	p.Save(500, 500, "test.png")
+	p.plot.Add(rssLine)
+	if err := p.plot.Save(500, 500, "test.png"); err != nil {
+		return fmt.Errorf("unable to save plot: %s %v", opts.Filename, err)
+	}
+	return nil
 }
